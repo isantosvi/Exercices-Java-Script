cfdea9fc3e5f06847ff4a1c405c8a041c07d5f49
isantosvi <117127429+isantosvi@users.noreply.github.com>
2024-05-15 20:24:43 -0300

    Add files via upload

---
+++ Exercicios/JavaScript10.js
@@ -0,0 +1,5 @@
+var distanciapercorrida = Number(prompt("Digite a distância percorrida em km"));
+var tempo = Number(prompt("Digite o tempo gasto em horas"));
+
+var velocidademedia = distanciapercorrida / tempo;
+console.log(velocidademedia)
