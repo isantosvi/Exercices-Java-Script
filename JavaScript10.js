5e1a45ce5cd1ef61c32d7b11066a355b09cbed58
isantosvi <117127429+isantosvi@users.noreply.github.com>
2024-05-16 12:52:59 -0300

    Delete JavaScript10.js

--- JavaScript10.js
+++
@@ -1,5 +0,0 @@
-var distanciapercorrida = Number(prompt("Digite a distância percorrida em km"));
-var tempo = Number(prompt("Digite o tempo gasto em horas"));
-
-var velocidademedia = distanciapercorrida / tempo;
-console.log(velocidademedia)
