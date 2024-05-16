fddf209e4e2b10b513d6a3668d71737ef7d0ed1e
isantosvi <117127429+isantosvi@users.noreply.github.com>
2024-05-16 12:53:20 -0300

    Delete JavaScript12.js

--- JavaScript12.js
+++
@@ -1,5 +0,0 @@
-var valorCompra = Number(prompt("Digite o valor da compra"));
-var quantidadeParcela = Number(prompt("Digite a quantidade de parcelas"));
-
-var valorPrestacao = valorCompra / quantidadeParcela;
-console.log(valorPrestacao);
