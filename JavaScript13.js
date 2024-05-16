0303e68a86c6c5cc6388a4338ec6c9f1d81b36d2
isantosvi <117127429+isantosvi@users.noreply.github.com>
2024-05-16 12:53:31 -0300

    Delete JavaScript13.js

--- JavaScript13.js
+++
@@ -1,6 +0,0 @@
-var valorAdquiridoLoja = Number(prompt("Informe o valor do produto adquirido na loja:"));
-
-var percentualLucro= Number(prompt("Informe o percenual de lucro desejado:"));
-
-var valorFinal = ((valorAdquiridoLoja * percentualLucro) / 100) + valorAdquiridoLoja;
-console.log(valorFinal);
